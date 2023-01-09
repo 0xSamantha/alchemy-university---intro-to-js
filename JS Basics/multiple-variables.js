be4f13a3be1521eb2700875a42c162e44b8c6c66
samcmcr <samchavoya@gmail.com>
2023-01-08 20:04:39 -0800

    multiple variables

---
+++ JS Basics/multiple-variables.js
@@ -0,0 +1,7 @@
+// Create two variables a and b with the same value in each. Similar to the example above, you can first declare a and then set b equal to a.
+
+const a = 5;
+const b = a;
+
+// module.exports = { a, b }
+
