4d7fab6016b45fa8b698835ea58860eff7faf801
samcmcr <samchavoya@gmail.com>
2023-01-10 22:09:43 -0800

    strings

---
+++ JS Basics/strings.js
@@ -0,0 +1,6 @@
+// In the variable a is stored the value "World". Your task is to store "Hello World" in the variable b.
+
+const a = "World";
+const b = "Hello " + a;
+
+// module.exports = { a,b }
